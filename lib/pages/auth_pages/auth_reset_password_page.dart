c41444e594f4e3763ab97a0c513da0a6f0be80e3
Alexey Shishlyannikov <alexeyshishlyannikov@Alexeys-MacBook-Pro.local>
2019-01-23 19:44:09 -0800

    Added reset password text widget

--- lib/pages/auth_pages/auth_reset_password_page.dart
+++ lib/pages/auth_pages/auth_reset_password_page.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 
 class AuthResetPasswordPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final _passwordTextController = TextEditingController();
+
   Widget _buildPasswordTextField() {
-    return Container();
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      controller: _passwordTextController,
+      validator: (String input) {
+        if (input.length < 6) {
+          return 'Password should be at least 6 characters.';
+        }
+      },
+    );
   }
 
   Widget _buildConfirmPasswordTextField() {
-    return Container();
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Confirm Password'),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      validator: (String input) {
+        if (_passwordTextController.text != input) {
+          return 'Passwords don\'t match.';
+        }
+      },
+    );
   }
 
   Widget _buildSubmitButton() {
-    return Container();
+    return RaisedButton(
+      child: Text('Reset Password'),
+      onPressed: _submitForm,
+    );
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
   }
 
   @override
